362ec07f39968e974391b3b7407938d02f7a7013
Pawel Ignaczak <ignak265@gmail.com>
2024-09-25 10:03:00 +0200

    Build/ Initial commit

---
+++ src/main/java/pawel/cookier/ignaczak/cookierslib/CookiersLib.java
@@ -0,0 +1,17 @@
+package pawel.cookier.ignaczak.cookierslib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CookiersLib extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
