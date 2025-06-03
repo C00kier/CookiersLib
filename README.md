6ec6733c4bdb26c54d3b33919c93f09223827dee
Pawel Ignaczak <ignak265@gmail.com>
2025-06-03 11:11:55 +0200

    Feature/ added java doc for ValidationUtilityImpl

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## CookiersLib 1.0.9o
+## CookiersLib 1.0.9p
 
 It is a library containing the most frequently repeated functions during my adventure with writing plugins.
 
--- src/main/java/pawel/cookier/ignaczak/cookierslib/world/WorldUtilityImpl.java
+++ src/main/java/pawel/cookier/ignaczak/cookierslib/world/WorldUtilityImpl.java
@@ -5,8 +5,17 @@ import org.bukkit.World;
 import org.bukkit.entity.Player;
 import pawel.cookier.ignaczak.cookierslib.repositories.world.WorldUtility;
 
+/**
+ * Implementation of {@link WorldUtility} for interacting with world-related settings and utilities.
+ */
 public class WorldUtilityImpl implements WorldUtility {
 
+    /**
+     * Checks whether the {@link GameRule#KEEP_INVENTORY} is enabled in the player's current world.
+     *
+     * @param player the player whose world to check
+     * @return true if keepInventory is enabled, false if disabled or undefined
+     */
     @Override
     public boolean isKeepInventoryEnabledOnPlayersWorld(Player player) {
         World world = player.getWorld();
